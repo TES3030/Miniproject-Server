8be87400303fbed27ba188eb497fb1fac09b87a4
Nunukuluk <nkajs16@student.aau.dk>
2017-10-23 17:10:19 +0200

    Getting the IP-address

--- src/Server.java
+++ src/Server.java
@@ -4,29 +4,36 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
-/**
- * Created by cocok on 23-10-2017.
- */
 public class Server {
-<<<<<<< HEAD
 
-=======
     public static void main(String[] args) {
 
-        try{
-            Socket Socket = new Socket(InetAddress.getByName("localhost"),50000);
-            PrintWriter out = new PrintWriter(Socket.getOutputStream(),true);
-            BufferedReader in= new BufferedReader(new InputStreamReader(Socket.getInputStream()));
+        try {
+            Socket Socket = new Socket(InetAddress.getByName("localhost"), 50000);
+            PrintWriter out = new PrintWriter(Socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(Socket.getInputStream()));
             out.println("I am a client");
             String l = in.readLine();
             System.out.println(l);
             in.close();
             out.close();
             Socket.close();
-        }catch(IOException e){
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Getting the IP address
+        InetAddress ip;
+        try {
+
+            ip = InetAddress.getLocalHost();
+            System.out.println("Current IP address : " + ip.getHostAddress());
+
+        } catch (UnknownHostException e) {
+
             e.printStackTrace();
         }
     }
->>>>>>> 2a68e20655f2013426b42823f52648f3dc3a12ad
 }
