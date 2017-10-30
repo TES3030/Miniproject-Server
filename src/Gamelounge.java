2bf50e3f3cec62ebb58ff3c0ccca750d355eca73
Z4IB0T <Z4IB0T@users.noreply.github.com>
2017-10-30 10:05:21 +0100

    gameloung and playerobjects yay

--- src/Gamelounge.java
+++ src/Gamelounge.java
@@ -4,19 +4,50 @@ import java.io.IOException;
  * Created by Tobias on 29/10/2017.
  */
 public class Gamelounge {
+
+
+
+    //array of player-objects,
+        //each with a nickname
+        //ready bool
+        //ip address
+
+
+
     static void clientInfo() throws IOException {
+
         //system.out means sending this text to client
         // Introduction to the game lounge
         System.out.println("---------------------------------------------------------------");
         System.out.println("Welcome to the game lounge! Here you can see all players who have joined \n " +
                 "Wait here until someone starts the game. Type \"start\" if you want the game to start :)");
         System.out.println("---------------------------------------------------------------");
-        // display when a new client joins
-        System.out.println("A new player joined the lounge!");
-        System.out.println("The players currently in the lounge are:\n" + /*clientThreadName + */"\n");
+
 
         //Check start booleans for every client
         //If 100% has their start booleans = start, startgame + gameHasStarted + gameRunning
 
     }
+
+    static boolean areClientsReady(){
+        //check start bools for all playerobjects
+
+        if(false)//if all = true, return true
+            return true;
+        else//else return false
+            return false;
+    }
+
+    static void clientJoins(){
+        //client sends nickname and ip address
+
+        //make new player object with those 2 data
+        //add new player to array
+        //print array of joined players
+
+        // display when a new client joins
+        System.out.println("A new player joined the lounge!");
+        System.out.println("The players currently in the lounge are:\n" + /*nickname + */"\n");
+    }
+
 }
