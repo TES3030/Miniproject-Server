8bfe8a00f91a167d1d21c6df51f607bf83d4698c
Z4IB0T <Z4IB0T@users.noreply.github.com>
2017-10-31 10:38:45 +0100

    last details

--- src/Client.java
+++ src/Client.java
@@ -49,11 +49,11 @@ public class Client {
             if(Objects.equals(s, "connect")) {//this returns true no matter the string - wtf
                 //Wait for user input
                 try {
-<<<<<<< HEAD
-                    System.out.println("Write the ip you want to connect to");
-=======
 
->>>>>>> master
+                    System.out.println("Write the ip you want to connect to");
+
+
+
                     IPAdress = input.nextLine(); //Read the IP address
 
                     //connect to the IP address given.
