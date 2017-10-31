ef52c1d8acc30ffbae484d9401e3364e069efbb3
Nunukuluk <nkajs16@student.aau.dk>
2017-10-31 12:18:31 +0100

    Something in the client

--- src/ServerThread.java
+++ src/ServerThread.java
@@ -74,12 +74,12 @@ public class ServerThread extends Thread {
             // while there is a message from the client, print it
 
             // Should be something like this, but won't work unless client is changed quite a bit
-            while(IPAddress != Inet4Address.getLocalHost().getHostAddress()) {
+            /*while(IPAddress != Inet4Address.getLocalHost().getHostAddress()) {
                 IPAddress = in.readLine();
                 if(IPAddress != Inet4Address.getLocalHost().getHostAddress()) {
                     out.println("\nBro, we don't recognize that IP. Please try again");
                 }
-            }
+            }*/
 
             nickName = in.readLine();
 
